a5948037753b670649bb92ada75dfcd4c514adb8
Valokaari <59644252+Valokaari@users.noreply.github.com>
2025-02-25 14:58:58 +0200

    Update script.js
    Poistettu turha juttu

--- Pay-As-You-Go/front/script.js
+++ Pay-As-You-Go/front/script.js
@@ -356,14 +356,7 @@ function adjustFlagPosition(flag, offset) {
     console.log(`Moved flag to top: ${flag.style.top}`);
 }
 
-// Apufunktio tarkistaa, menevätkö kaksi lippua päällekkäin
-function checkOverlap(flag1, flag2) {
-    const rect1 = flag1.getBoundingClientRect();
-    const rect2 = flag2.getBoundingClientRect();
 
-    return rect1.left < rect2.right && rect1.right > rect2.left &&
-           rect1.top < rect2.bottom && rect1.bottom > rect2.top;
-}
 
 // Funktio vaihtaa tumma tila päälle ja pois
 function toggleDarkMode() {
